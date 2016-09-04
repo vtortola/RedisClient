952b25ab46484533274ce0ef2bf7ee215d3f0095
Valeriano Tortola <vtortola@gmail.com>
2016-09-04 02:03:54 +0100

    Assembly version

--- vtortola.RedisClient/Properties/AssemblyInfo.cs
+++ vtortola.RedisClient/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("vtortola.RedisClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Another .NET Redis client, for the craic ")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("http://vtortola.github.io")]
 [assembly: AssemblyProduct("vtortola.RedisClient")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("vtortola")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 [assembly: InternalsVisibleTo("UnitTest.RedisClient")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
