9ecc85bfe18dc15b07d0470b95b1d9261c166f95
Valeriano Tortola <vtortola@gmail.com>
2016-09-16 00:39:20 +0100

    Version 0.0.3-alpha

--- vtortola.RedisClient/Properties/AssemblyInfo.cs
+++ vtortola.RedisClient/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("http://vtortola.github.io")]
 [assembly: AssemblyProduct("vtortola.RedisClient")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("MIT © 2016")]
 [assembly: AssemblyTrademark("vtortola")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.2.*")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: AssemblyVersion("0.0.3.*")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
 [assembly: InternalsVisibleTo("UnitTest.RedisClient")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("proc-dbg-launcher")]
