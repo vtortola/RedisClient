bb400ed4e224c9843444e191dd7199bfe8ab0b97
Valeriano Tortola <vtortola@gmail.com>
2016-09-19 23:48:05 +0100

    Removing modal caches

--- TestApplications/SimpleQA/SimpleQA.WebApp/Scripts/app.global.errorhandling.js
+++ TestApplications/SimpleQA/SimpleQA.WebApp/Scripts/app.global.errorhandling.js
@@ -9,8 +9,14 @@ simpleqa.global.errorhandling = function ($) {
             $genericErrorModal.find('.modal-body').text(lastError)
         });
 
+        // removing modal caches
+        $('body').on('hidden.bs.modal', '.modal', function () {
+            $(this).removeData('bs.modal');
+        });
+
         $(document).ajaxError(function (event, jqxhr, settings, thrownError) {
             lastError = thrownError;
+            $('.modal').modal('hide'); // close opened modals
             $genericErrorModal.modal('show');
         });
 }
