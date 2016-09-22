3cba63d2b64a35bc6ff2d7faaacbe890f80c1619
Valeriano Tortola <vtortola@gmail.com>
2016-09-22 21:45:41 +0100

    Visit question procedure

---
+++ TestApplications/SimpleQA/SimpleQA.RedisCommands/CommandExecuter/VisitQuestion/VisitQuestionCommandExecuter.cs
@@ -0,0 +1,24 @@
+﻿using SimpleQA.Commands;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using vtortola.Redis;
+
+namespace SimpleQA.RedisCommands
+{
+    public class VisitQuestionCommandExecuter : ICommandExecuter<VisitQuestionCommand, VisitQuestionCommandResult>
+    {
+        readonly IRedisChannel _channel;
+
+        public VisitQuestionCommandExecuter(IRedisChannel channel)
+        {
+            _channel = channel;
+        }
+
+        public Task<VisitQuestionCommandResult> ExecuteAsync(VisitQuestionCommand command, IPrincipal user, CancellationToken cancel)
+        {
+            _channel.Dispatch("VisitQuestion {question} @QuestionId @Views", command);
+            return Task.FromResult(VisitQuestionCommandResult.Nothing);
+        }
+    }
+}
