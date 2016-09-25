2b69bae8ef1bd463fd97568e3aaf5462d9c061ce
Valeriano Tortola <vtortola@gmail.com>
2016-09-25 12:45:03 +0100

    Notify answers only from other users.

--- TestApplications/SimpleQA/SimpleQA.RedisCommands/CommandExecuter/Answer/AnswerCreateCommandExecuter.cs
+++ TestApplications/SimpleQA/SimpleQA.RedisCommands/CommandExecuter/Answer/AnswerCreateCommandExecuter.cs
@@ -31,7 +31,7 @@ namespace SimpleQA.RedisCommands
             var quesionData = result[1].GetStringArray();
             var slug = quesionData[0];
             var ownerId = quesionData[1];
-            //if (ownerId != user.GetSimpleQAIdentity().Id)
+            if (ownerId != user.GetSimpleQAIdentity().Id)
             {
                 _channel.Dispatch("NotifyQuestionUpdate {user} @qid @uid",
                                   new
