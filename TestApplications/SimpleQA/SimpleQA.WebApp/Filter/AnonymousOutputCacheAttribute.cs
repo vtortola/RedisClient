fa013ea0b68189e2773282031948829adcaeb6f4
Valeriano Tortola <vtortola@gmail.com>
2016-10-01 10:43:11 +0100

    Output caching for anonymous users

--- TestApplications/SimpleQA/SimpleQA.WebApp/Filter/AnonymousOutputCacheAttribute.cs
+++ TestApplications/SimpleQA/SimpleQA.WebApp/Filter/AnonymousOutputCacheAttribute.cs
@@ -11,6 +11,7 @@ namespace SimpleQA.WebApp.Filter
     {
         public AnonymousOutputCacheAttribute()
         {
+            // Bug in OutputCache profiles? Even if Location is set to
             // Server in the config, I can see Cache-Control: public in the response...
             // because the Location property is initialized to Any ignoring the Web.config.
 
